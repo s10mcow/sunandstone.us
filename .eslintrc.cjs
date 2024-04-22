8f3220c14785a4f3e893f251d7ac58994af056a5
Sten Muchow <sten.muchow@gmail.com>
2024-04-22 12:43:15 -0400

    fix: wip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,25 @@
 module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
   "extends": [
-    "@thesparklaboratory/eslint-config-react"
-  ]
-}
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
+  ],
+  "plugins": ["@typescript-eslint", "unused-imports"],
+  "root": true,
+  "rules": {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "no-console": "error",
+    "react/no-unescaped-entities": "off"
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
+};
 
