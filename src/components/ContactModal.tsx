02851468bf9137e12e269ac74f149b85c4969ebd
Sten Muchow <sten.muchow@gmail.com>
2024-04-22 14:24:12 -0400

    fix: wip

--- src/components/ContactModal.tsx
+++ src/components/ContactModal.tsx
@@ -5,7 +5,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 export default function ContactModal({
   isOpen,
